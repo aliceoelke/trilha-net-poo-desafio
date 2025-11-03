0539d2db8eaed17c44e44ae38e1b625a4b51ee24
Alice Oelke <oelkealice2016@gmail.com>
2025-11-03 17:48:02 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 110", imei: "37999999999", memoria: 32);
+Console.WriteLine("Iniciando smartphone Nokia: ");
+nokia.Ligar();
+nokia.InstalarAplicativo("Meu Alelo");
+
+Smartphone iphone = new Iphone(numero: "987654", modelo: "iPhone 13", imei: "48999999999", memoria: 256);
+Console.WriteLine("Iniciando smartphone Iphone: ");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iFood");
